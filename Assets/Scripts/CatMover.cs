d596f61702bed9bd58a9bf0e2f26d33a569a327b
Wizcas <wizcas@qq.com>
2017-04-22 15:09:46 +0800

    simple nav movements and jump

--- Assets/Scripts/CatMover.cs
+++ Assets/Scripts/CatMover.cs
@@ -5,16 +5,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class CatMover : MonoBehaviour {
+[RequireComponent(typeof(NavMeshAgent))]
+public class CatMover : MonoBehaviour
+{
+    private const float EPSILON = .0025f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 2f;
+
+    private NavMeshAgent _agent;
+
+    void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Messenger.AddListener<Vector3>("MoveTo", SetDestination);
+    }
+
+    public void SetDestination(Vector3 dest)
+    {
+        // too near to move
+        if (IsAtSamePosition(dest, transform.position))
+        {
+            return;
+        }
+        _agent.destination = dest;
+    }
+
+    private float SqrDistance(Vector3 a, Vector3 b)
+    {
+        return (a - b).sqrMagnitude;
+    }
+
+    private bool IsAtSamePosition(Vector3 a, Vector3 b)
+    {
+        return SqrDistance(a, b) <= EPSILON;
+    }    
 }
---
+++ Assets/Scripts/ClickInput.cs
@@ -0,0 +1,30 @@
+﻿/************************************
+** Created by Wizcas (wizcas.me)
+************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickInput : MonoBehaviour
+{
+    public float raycastDistance = 20f;
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleClick();
+        }
+    }
+
+    private void HandleClick()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, raycastDistance, 1 << LayerMask.NameToLayer("Environment")))
+        {
+            Debug.LogFormat("hit on: {0}", hit.collider.name);
+            Messenger.Broadcast<Vector3>("MoveTo", hit.point);
+        }
+    }
+}
