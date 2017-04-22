79c6e64a435fcffb30d44412b76fbc0369a85d23
Wizcas <wizcas@qq.com>
2017-04-22 13:06:29 +0800

    building up a test scene

---
+++ Assets/Scripts/CatMover.cs
@@ -0,0 +1,20 @@
+﻿/************************************
+** Created by Wizcas (wizcas.me)
+************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
